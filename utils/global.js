11f514eaa9ac632f4137497bafb8f694eaa0cc7c
feb <dinhhien102@gmail.com>
2021-10-03 17:21:01 +1100

    Updated domain from imfeb.me to febis.me

--- utils/global.js
+++ utils/global.js
@@ -4,11 +4,11 @@
   access any variable via object syntax global.yourVarName
 */
 export default {
-  siteUrl: 'https://imfeb.me',
+  siteUrl: 'https://febis.me',
   siteName: 'I`m Feb',
   author: 'Feb Dao',
   githubURL: 'https://github.com/febdao',
   siteTitle: 'I\'m Feb, I love web development and photography',
   siteDesc: 'Welcome to my blog, where I will share my experiences and knowledge about Web front-end development.',
-  mainImage: 'https://imfeb.me/images/feb.jpg',
+  mainImage: 'https://febis.me/images/feb.jpg',
 };
