2eb84d9a8848df7b13d2f87c8677af7622626112
feb <dinhhien102@gmail.com>
2021-10-27 22:51:25 +1100

    DEV: Updated the gtag

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -1,11 +1,12 @@
-import Vue from 'vue'
-import VueGtag from 'vue-gtag'
+import Vue from 'vue';
+import VueGtag from 'vue-gtag';
+
 export default ({ app }) => {
   Vue.use(
     VueGtag,
     {
-      config: { id: process.env.GTAG },
-      appName: 'FebIsMe',
+      config: { id: 'G-6J1WPEX5MV' },
+      appName: 'blog',
       enabled: true,
       pageTrackerScreenviewEnabled: true,
     },
