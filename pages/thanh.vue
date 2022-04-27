dcfb2a8e66a29cba5d9abee7453bb1786ed52d8f
feb <dinhhien102@gmail.com>
2022-04-27 17:32:52 +1000

    GAME: Update layout

--- pages/thanh.vue
+++ pages/thanh.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="p-2 md:pt-20 pt-10 pb-20">
-    <div class="container mx-auto">
-      <div v-if="isDone">
+  <div class="fixed inset-0 bg-gray-400 z-10 bg-center bg-cover bg-no-repeat h-screen" style="background-image: url('/images/thanhvu.jpg');">
+    <div class="absolute inset-0 z-9 min-h-screen bg-black opacity-60 ">
+    </div>
+    <div class="absolute w-full
+    h-screen
+    bg-gradient-to-r
+    from-pink-500
+    via-red-500
+    to-yellow-500
+    background-animate opacity-20 inset-0 z-0"></div>
+    <div v-if="isDone">
+      <div class="container mx-auto mt-20 p-20 bg-green-800 relative z-50 bg-opacity-80">
         <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           <li v-for="(card, index) in cards" :key="card.text">
             {{ index  + 1 }}. {{ card.text }}
           </li>
         </ul>
         <p class="text-3xl my-4 mt-20 w-full text-center">
-          <button @click="reset()" class="button bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 background-animate white inline-block w-250 opacity-100 lg:px-20 px-10 py-2">Reset</button>
+          <button @click="reset()" class="button bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 background-animate white inline-block w-250 opacity-100 lg:px-20 px-10 py-2 relative z-40">Reset</button>
         </p>
       </div>
-      <div v-else>
-        <div v-if="isShow">
+    </div>
+    <div v-else>
+      <div v-if="isShow">
+        <div class="container mx-auto p-10 md:p-20">
           <div class="carousel-wrapper">
             <client-only>
               <slick :options="slickOptions">
@@ -26,28 +37,20 @@
             </client-only>
           </div>
           <p class="text-3xl my-4 w-full text-center">
-            <button @click="endShow()" class="button bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 background-animate white inline-block w-250 opacity-100 lg:px-20 px-10 py-2">Done</button>
+            <button @click="endShow()" class="button bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 background-animate white inline-block w-250 opacity-100 lg:px-20 px-10 py-2  relative z-40">Done</button>
           </p>
         </div>
-        <div v-else>
-          <div class="bg-center bg-cover bg-no-repeat h-screen items-center justify-center flex flex-col relative" style="background-image: url('/images/thanhvu.jpg');">
-
-            <div class="absolute inset-0 z-9 min-h-screen bg-black opacity-60 ">
-            </div>
-            <div class="absolute inset-0 z-10 min-h-screen flex items-center text-white ">
-              <div class="absolute w-full
-              h-screen
-              bg-gradient-to-r
-              from-pink-500
-              via-red-500
-              to-yellow-500
-              background-animate opacity-20 inset-0 z-0"></div>
-              <div class="w-full px-10 md:px-24 z-10">
-                  <h1 class="md:flex text-3xl md:text-5xl font-bold text-left tracking-wide">Chúc mừng sinh nhật</h1>
-                  <h2 class="md:flex text-4xl md:text-7xl font-bold text-white text-left tracking-wide">Thành Vũ</h2>
-                  <p class="text-3xl my-4">
-                    <button @click="showTime()" class="button bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 background-animate white inline-block w-250 opacity-100 lg:px-20 px-10 py-2">Let's play!!!</button>
-                  </p>
+      </div>
+      <div v-else>
+        <div class="h-screen items-center justify-center flex flex-col relative">
+          <div class="absolute inset-0 z-10 min-h-screen flex items-center text-white ">
+            <div class="w-full px-10 md:px-24 z-10">
+              <div class="container mx-auto p-10 md:p-20">
+                <h1 class="md:flex text-3xl md:text-5xl font-bold text-left tracking-wide">Chúc mừng sinh nhật</h1>
+                <h2 class="md:flex text-4xl md:text-7xl font-bold text-white text-left tracking-wide">Thành Vũ</h2>
+                <p class="text-3xl my-4">
+                  <button @click="showTime()" class="button bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 background-animate white inline-block w-250 opacity-100 lg:px-20 px-10 py-2">Let's play!!!</button>
+                </p>
               </div>
             </div>
           </div>
